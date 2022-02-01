f6108cf99d0735fbd39273f78627f34bac5451ce
JoaoRafa19 <joaopedrorafael19@gmail.com>
2022-02-01 14:31:16 -0300

    configurações

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  FILE *file, *outputfile;
+
+  while (1)
+  {
+  }
+
+  return 0;
+}
---
+++ src/utils/utils.c
@@ -0,0 +1,28 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+#ifdef WIN32
+#define cls() system("cls");
+#else
+#define cls() system("clear");
+#endif
+
+struct tm *currentTime()
+{
+
+  time_t rawtime;
+  struct tm *timeinfo;
+  time(&rawtime);
+  timeinfo = localtime(&rawtime);
+  return timeinfo;
+}
+
+void menu()
+{
+  puts("Opcoes:");
+  puts("0 - Sair");
+  puts("1 - Ler arquivo de testes");
+  puts("2 - Entradas por teclado");
+}
